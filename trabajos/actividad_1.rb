120726a100cb63b7f060ce74d3db1f95b1e137aa
Yony Brinez <arleybri18@gmail.com>
2023-03-09 07:28:23 -0500

    SE AÑADEN COMENTARIOS REALIZADOS AL CODIGO

--- trabajos/actividad_1.rb
+++ trabajos/actividad_1.rb
@@ -27,23 +27,30 @@ def reporte_usuarios
     lineas_archivo = leer_archivo_entero
     # imprimir lo que tiene lineas_archivo
 end
-
+#SE SOLICTA AL USURIO POR MEDIO DE UN MENU QUE DESEA REALIZAR
 print "CON EL SIGUIENTE  MENU  QUE OPCION DESEAS REALIZAR \n\n"
+#SE CREA UN CICLO DONDE LOS NUMEROS NO SEAN DIFERENTES DE 3
 while opcion_1 != 3
- 
+ #SE CREAN LAS OPCIONES A TRABAJAR CON EL USUARIO Y SE ALMACENA 
+ #LA INFORMACION EN VARIABLE
     print "1. REGISTRAR  USUARIOS \n"
     print "2. VER USUARIOS REGISTRADOS\n"
     print "3. SALIR\n"
     opcion_1 = gets.chomp.to_i
-
+#SE CREA UNA CONDICION DONDE SE VALIDA SI LA OPCION ES IGUAL
+#A 3 SALGA 
     if opcion_1 == 3
         break
     end
-
+#SE CREA UNA CONDICION DONDE SE VALIDA SI LA OPCION ES IGUAL A 1
+#SOLICITE AL USUARIO CUANTOS DESEA REGISTAR. Y SE ALMACENA EN UNA VARIABLE
     if opcion_1 == 1
         print "INGRESE CUENTOS USUARIOS DESEA REGISTRAR\n\n"
         opcion_2 = gets.chomp.to_i
+#SE CREA UN ARRAY DONDE SE ALMACENARAN LOS DATOS INGRESADOS        
         usuarios = []
+#SE CREA UN CICLO DONDE SE ITERA Y SE ALMACENA EN UN HASH LAS INFORMACION 
+#INGRESADO POR EL USUARIO
         opcion_2.times do
             usuario = {}
             print " INGRESE SU NOMBRE \n"
@@ -61,10 +68,10 @@ while opcion_1 != 3
             usuario["dia"]= dia
             usuario["año"]= año
             usuario["edad"]= edad
-
+#SE REALIZA UN PUSH DE LOS DATOS INGRESADOS AL ARRAY CREADO 
             usuarios.push(usuario)
         end
-
+#SE CREA METODO PARA CREACION Y ADICION DE FILA  DE INFORME Y CREAR LOS HEADER DE LOS INFORMES
         File.open("usuarios.txt", 'a') do |file|
             usuarios.each do |usuario|
                 linea = "#{usuario["nombre"]},#{usuario["apellido"]},#{usuario["dia"]},#{usuario["edad"]}\n"
@@ -73,14 +80,15 @@ while opcion_1 != 3
         end
 
 
-
+#SI LA OPCION DADA ES 2 IMPRIMIR POR PANTALLA SI NO HAY REGISTROS 
+#O IMPRIMIER REGISTROAS SI YA HAY 
     elsif opcion_1 == 2
         puts "NO HAY USUARIOS REGISTRADOS INTENTA DE NUEVO" 
         #puts reporte_usuarios
         reporte_usuarios
 
     else
-
+#DE LO CONTRARIO SE IMPRIMIRA NO HAY USUARIOS REGISTRADOS
         puts "NO HAY USUARIOS REGISTRADOS INTENTA DE NUEVO" 
         
     end    
