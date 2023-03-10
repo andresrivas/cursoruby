efb57f1d1ed4cc20b13bc5a7a77c55d3de5709a6
Yony Brinez <arleybri18@gmail.com>
2023-03-10 08:33:35 -0500

    trabajo de metodos getter and setter

---
+++ objetos/calculadora.rb
@@ -0,0 +1,39 @@
+
+class Calculadora
+    def initialize(num1, num2)
+        @num1 = num1
+        @num2 = num2
+    end
+    def suma
+        @num1 + @num2 
+    end
+    def resta
+        @num1 - @num2 
+    end
+    def multiplicacion
+        @num1 * @num2 
+    end
+    def division
+        @num1 / @num2.to_f 
+    end
+end
+
+resultado = Calculadora.new(7, 8)
+#para retornar el resultado de la operacion 
+#impresa en pantalla con puts de lo contrario solo realiza la
+#operacion pero no la muestra
+puts "El resultado de la suma es #{resultado.suma}"
+puts "El resultado de la resta es #{resultado.resta}"
+puts "El resultado de la multiplicacion es #{resultado.multiplicacion}"
+puts "El resultado de la division es #{resultado.division}"
+puts "#" * 30
+puts "#" * 30
+
+resultado2 = Calculadora.new(9, 10)
+#para retornar el resultado de la operacion 
+#impresa en pantalla con puts de lo contrario solo realiza la
+#operacion pero no la muestra
+puts "El resultado de la suma es #{resultado2.suma}"
+puts "El resultado de la resta es #{resultado2.resta}"
+puts "El resultado de la multiplicacion es #{resultado2.multiplicacion}"
+puts "El resultado de la division es #{resultado2.division}"
---
+++ objetos/setter_getter.rb
@@ -0,0 +1,16 @@
+class Calculadora
+    def initialize(num1, num2)
+        @num1 = num1
+        @num2 = num2
+    end
+
+    #setter cambia valores del atributo
+    def num1 = (a)
+        @num1 = 0
+    end
+
+    #getter muestra los valores del atributo
+    def num1
+        @num1 
+    end
+end
